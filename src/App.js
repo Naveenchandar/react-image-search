b41ded581885dc146339a6db5ba3ca17846d698a
Naveenchandar <naveenchandece@gmail.com>
2020-07-27 10:48:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchBar from './Components/SearchBar';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props) 
     this.state = {
       gallery: [],
       currentQuery: '',
